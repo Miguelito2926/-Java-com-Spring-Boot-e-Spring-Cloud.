7967752fee4dd893ed892da99f8a3949e12cc878
Miguelito2926 <ednaldo.junior@prill.com.br>
2024-08-08 12:25:46 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/ednaldo/hr_worker/resources/WorkerResource.java
@@ -0,0 +1,34 @@
+package com.ednaldo.hr_worker.resources;
+
+import com.ednaldo.hr_worker.entities.Worker;
+import com.ednaldo.hr_worker.repositories.WorkerRepository;
+import org.hibernate.jdbc.Work;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/workers")
+public class WorkerResource {
+
+    @Autowired
+    private WorkerRepository workerRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Worker>> findAll() {
+        List<Worker> list = workerRepository.findAll();
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Worker> findById(@PathVariable Long id) {
+        Worker optional = workerRepository.findById(id).get();
+        return ResponseEntity.ok(optional);
+    }
+}
---
+++ hr-worker/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into worker(name, daily_Income) values('Ednaldo', 200.0);
+insert into worker(name, daily_Income) values('Dione', 300.0);
+insert into worker(name, daily_Income) values('Miguel', 250.0);
