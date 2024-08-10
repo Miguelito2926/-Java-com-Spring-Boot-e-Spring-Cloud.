8f5fe781cefe5eb7b3e9971de0bb495fa5be7936
Miguelito2926 <ednaldo.junior@prill.com.br>
2024-08-10 10:24:12 -0300

    feat: Hystrix fault tolerance

--- hr-worker/src/main/java/com/ednaldo/hr_worker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/ednaldo/hr_worker/resources/WorkerResource.java
@@ -37,6 +37,12 @@ public class WorkerResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
 
+//        try{
+//            Thread.sleep(3000L);
+//        }catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
         logger.info("PORT = " + environment.getProperty("local.server.port"));
         Worker optional = workerRepository.findById(id).get();
         return ResponseEntity.ok(optional);
