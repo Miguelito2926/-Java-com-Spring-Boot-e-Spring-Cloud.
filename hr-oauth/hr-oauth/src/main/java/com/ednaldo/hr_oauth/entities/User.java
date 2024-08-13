e0a56fce556bd726188097de9848ea26c2a33a0d
Miguelito2926 <ednaldo.junior@prill.com.br>
2024-08-13 10:25:48 -0300

    feat: implementation oauth part 1

--- hr-oauth/hr-oauth/src/main/java/com/ednaldo/hr_oauth/entities/User.java
+++ hr-oauth/hr-oauth/src/main/java/com/ednaldo/hr_oauth/entities/User.java
@@ -1,12 +1,19 @@
 package com.ednaldo.hr_oauth.entities;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 
-public class User implements Serializable {
+public class User implements UserDetails, Serializable {
     private static final long serialVersionUID = 1L;
 
 
@@ -51,8 +58,38 @@ public class User implements Serializable {
         this.email = email;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(nameRole -> new SimpleGrantedAuthority(nameRole.getRoleName())).collect(Collectors.toList());
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public void setPassword(String password) {
