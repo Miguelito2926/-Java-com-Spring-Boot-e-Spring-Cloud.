9e8faa78b93c246e3bd88e96004f00ca6246d399
Miguelito2926 <ednaldo.junior@prill.com.br>
2024-08-25 21:03:57 -0300

    feat: hr payroll and oauth container

--- hr-oauth/hr-oauth/src/main/java/com/ednaldo/hr_oauth/config/AppConfig.java
+++ hr-oauth/hr-oauth/src/main/java/com/ednaldo/hr_oauth/config/AppConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 public class AppConfig {
 
-    @Value("${jwt.secret}")
+    @Value("${jwt.secret:MY-JWT-SECRET}")
     private String jwtSecret;
 
     @Bean
